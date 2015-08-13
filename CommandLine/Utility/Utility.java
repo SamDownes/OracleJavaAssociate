6e93ecdd4aac7d72f266b549207047ad605b8d8a
SamDownes <samuel.valhalla@gmail.com>
2015-08-13 15:07:31 +0100

    Create Utility.java
    This class holds the static and non-static methods that will be called in the StaticMain class.

---
+++ CommandLine/Utility/Utility.java
@@ -0,0 +1,26 @@
+package com.samdownes.ocja.other_stuff_for_static;
+
+
+
+
+public class Utility{
+
+	
+	public static void printSomeStatic(){
+
+		System.out.println("This is some static printing goin on!");
+
+
+	}
+
+
+	public void printSomeNonStatic(){
+
+		System.out.println("This is some non-static printing goin on!");	
+
+
+	}
+
+
+
+}// End of Utility
